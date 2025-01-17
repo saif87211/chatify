3c8240a501e1db01f9aabef4691dfeefc0411894
Saif Ali <saifali87211@gmail.com>
2025-01-17 15:48:01 +0530

    add morgan package for log

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,8 @@
-import { config } from "./config/config.js";
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import morgan from "morgan";
+import { config } from "./config/config.js";
 import { errorHandler } from "./middlewares/error.middleware.js";
 
 const app = express();
@@ -14,6 +15,7 @@ app.use(express.json({ limit: "20kb" }));
 app.use(express.urlencoded({ limit: "20kb", extended: true }));
 app.use(express.static("public"));
 app.use(cookieParser());
+app.use(morgan("dev"));
 
 //routes
 import userRoutes from "./routes/user.routes.js";
